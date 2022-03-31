78a2a3fcf83cfafea5904c00e1d44f1f9ba27ad8
QuentinGeerts <quentin.geerts@bstorm.be>
2022-03-31 23:49:12 +0200

    ArrayList complète

--- src/Theorie/Base/Collections/demoCollections.java
+++ src/Theorie/Base/Collections/demoCollections.java
@@ -1,6 +1,8 @@
 package Theorie.Base.Collections;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class demoCollections {
     public static void main(String[] args) {
@@ -31,47 +33,123 @@ public class demoCollections {
 
         System.out.println(" --- LIST --- ");
 
-        // Création d'une liste vide
-        ArrayList myArrayList = new ArrayList();
+        // ArrayList()
+        // > Créer une instance vide de la collection avec une capacité initiale de 10
 
-        // Remplissage de la liste
-        System.out.println(myArrayList.add(1));
-        System.out.println(myArrayList.add('2'));
-        System.out.println(myArrayList.add(true));
-        System.out.println(myArrayList.add(4));
+        List myDefaultArrayList = new ArrayList();
 
+        // ArrayList(Collection<? extends E> c)
+        // > Créer une instance contenant les éléments de la collection fournie en paramètre dans l'ordre obtenu en utilisant son iterator
 
-        int indexToFind = myArrayList.indexOf(false);
-        if (indexToFind != -1) myArrayList.remove(indexToFind);
+        ArrayList myArrayListBasedOnAnotherList = new ArrayList(myDefaultArrayList);
 
-        System.out.println("Récupération de l'item à l'indice 2 : " + myArrayList.get(2));
+        // ArrayList(int initialCapacity)
+        // > Créer une instance vide de la collection avec la capacité initiale fournie en paramètre
 
-        System.out.println("Nombre d'éléments dans la liste : " + myArrayList.size());
-
-        System.out.println("Affichage avec une structure Foreach");
-        for (Object element : myArrayList) {
-            System.out.println(element);
-        }
-
-        System.out.println("Affichage avec une méthode forEach");
-        myArrayList.forEach(element -> System.out.println(element));
-
-        // boolean add(Object)	Ajouter un élément à la fin du tableau
-        // boolean addAll(Collection)	Ajouter tous les éléments de la collection fournie en paramètre à la fin du tableau
-        // boolean addAll(int, Collection)	Ajouter tous les éléments de la collection fournie en paramètre dans la collection à partir de la position précisée
-        // void clear()	Supprimer tous les éléments du tableau
-        // void ensureCapacity(int)	Augmenter la capacité du tableau pour s'assurer qu'il puisse contenir le nombre d'éléments passé en paramètre
-        // Object get(index)	Renvoyer l'élément du tableau dont la position est précisée
-        // int indexOf(Object)	Renvoyer la position de la première occurrence de l'élément fourni en paramètre
-        // boolean isEmpty()	Indiquer si le tableau est vide
-        // int lastIndexOf(Object)	Renvoyer la position de la dernière occurrence de l'élément fourni en paramètre
-        // Object remove(int)	Supprimer dans le tableau l'élément fourni en paramètre
-        // void removeRange(int, int)	Supprimer tous les éléments du tableau de la première position fournie incluse jusqu'à la dernière position fournie exclue
-        // Object set(int, Object)	Remplacer l'élément à la position indiquée par celui fourni en paramètre
-        // int size()	Renvoyer le nombre d'éléments du tableau
-        // void trimToSize()	Ajuster la capacité du tableau sur sa taille actuelle
+        ArrayList myArrayListWithACapacity = new ArrayList(20);
 
 
+        /*
+            Méthodes
+         */
+
+        // boolean add(Object)
+        // > Ajouter un élément à la fin du tableau
+
+        System.out.println("\n--- boolean add(Object) ---\n");
+        System.out.println("Élément ajouté ? : " + myDefaultArrayList.add("a"));
+        System.out.println("Élément ajouté ? : " + myDefaultArrayList.add(1));
+        System.out.println("Élément ajouté ? : " + myDefaultArrayList.add(true));
+
+        System.out.println("\nmyDefaultArrayList : " + myDefaultArrayList);
+
+        // boolean addAll(Collection)
+        // > Ajouter tous les éléments de la collection fournie en paramètre à la fin du tableau
+
+        System.out.println("\n--- boolean addAll(Collection) ---\n");
+        System.out.println("Élémént(s) ajouté(s) : " + myDefaultArrayList.addAll(myDefaultArrayList));
+
+        System.out.println("\nmyDefaultArrayList : " + myDefaultArrayList);
+
+        // boolean addAll(int, Collection)
+        // > Ajouter tous les éléments de la collection fournie en paramètre dans la collection à partir de la position précisée
+
+        System.out.println("\n--- boolean addAll(int, Collection) ---\n");
+        System.out.println("Élémént(s) ajouté(s) : " + myDefaultArrayList.addAll(2, new ArrayList(Arrays.asList(2, 4, 5, 6))));
+
+        System.out.println("\nmyDefaultArrayList : " + myDefaultArrayList);
+
+        // Object get(index)
+        // > Renvoyer l'élément du tableau dont la position est précisée
+
+        System.out.println("\n--- Object get(index) ---\n");
+        System.out.println("Valeur à l'indice 5 : " + myDefaultArrayList.get(5));
+
+        // void ensureCapacity(int)
+        // > Augmenter la capacité du tableau pour s'assurer qu'il puisse contenir le nombre d'éléments passé en paramètre
+
+        // Ne pas faire
+
+        // int indexOf(Object)
+        // > Renvoyer la position de la première occurrence de l'élément fourni en paramètre
+
+        System.out.println("\n--- int indexOf(Object) ---\n");
+        System.out.println("Position du premier l'élément 1 dans la liste : " + myDefaultArrayList.indexOf(1));
+
+        // int lastIndexOf(Object)
+        // > Renvoyer la position de la dernière occurrence de l'élément fourni en paramètre
+
+        System.out.println("\n--- int lastIndexOf(Object) ---\n");
+        System.out.println("Position du dernier élément 1 dans la liste : " + myDefaultArrayList.lastIndexOf(1));
+
+        // boolean isEmpty()
+        // > Indiquer si le tableau est vide
+
+        System.out.println("\n--- boolean isEmpty() ---\n");
+        System.out.println(myDefaultArrayList.isEmpty());
+
+        // Object remove(int)
+        // > Supprimer dans le tableau l'élément fourni en paramètre
+
+        System.out.println("\n--- Object remove(int) ---\n");
+        System.out.println("Élément retiré : " + myDefaultArrayList.remove(0));
+        System.out.println(myDefaultArrayList);
+
+        // boolean removeAll(Collection)
+        // > Supprimer tous les éléments du tableau de la première position fournie incluse jusqu'à la dernière position fournie exclue
+
+        System.out.println("\n--- boolean removeAll(Collection) ---\n");
+        System.out.println( "Valeurs retirées ? " + myDefaultArrayList.removeAll(Arrays.asList("z")) );
+        System.out.println( "Valeurs retirées ? " + myDefaultArrayList.removeAll(Arrays.asList(1, true, "z")) );
+
+        System.out.println("\nmyDefaultArrayList : " + myDefaultArrayList);
+
+        // Object set(int, Object)
+        // > Remplacer l'élément à la position indiquée par celui fourni en paramètre
+
+        System.out.println("\n--- Object set(int, Object) ---\n");
+        System.out.println( "Ancienne valeur : " + myDefaultArrayList.set(0, 100) );
+
+        System.out.println("\nmyDefaultArrayList : " + myDefaultArrayList);
+
+        // int size()
+        // > Renvoyer le nombre d'éléments du tableau
+
+        System.out.println("\n--- int size() ---\n");
+        System.out.println( myDefaultArrayList.size() );
+
+        // void trimToSize()
+        // > Ajuster la capacité du tableau sur sa taille actuelle
+
+        // Ne pas faire
+
+        // void clear()
+        // > Supprimer tous les éléments du tableau
+
+        System.out.println("\n--- void clear() ---\n");
+        myDefaultArrayList.clear();
+
+        System.out.println("\nmyDefaultArrayList : " + myDefaultArrayList);
 
     }
 }
